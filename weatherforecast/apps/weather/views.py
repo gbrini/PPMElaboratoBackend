ba19fee13e1f6bc968b6856f871a6313d2760d17
guido <guido.brini99@gmail.com>
2026-05-30 11:40:54 +0200

    Aggiunta prima query id prova per il forecast

--- weatherforecast/apps/weather/views.py
+++ weatherforecast/apps/weather/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+class WeatherForecastView(APIView):
+    permission_classes = [ AllowAny ]
+
+    def get(self, request):
+        location = request.query_params.get('location')
+
+        return Response({
+            "message": f"You searched for {location}"
+        })
