b6d85eebcd021d51a175779b2104502d967cd372
guido <guido.brini99@gmail.com>
2026-05-31 17:07:44 +0200

    Aggiunta pagina admin forecast

--- weatherforecast/apps/weather/admin.py
+++ weatherforecast/apps/weather/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import WeatherQuery
+
+@admin.register(WeatherQuery)
+class WeatherQueryAdmin(admin.ModelAdmin):
+    list_display = ( 'location', 'forecast_date', 'temperature', 'condition', 'created_at', 'user', )
+
+    list_display_links = ( 'location', )
+
+    list_filter = ( 'user', 'forecast_date', )
+
+    search_fields = ( 'location', 'user__username', )
