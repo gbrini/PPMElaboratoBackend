3b622b52b68ba482d568d025af1001aebdad86ca
guido <guido.brini99@gmail.com>
2026-05-31 15:20:47 +0200

    Validazione post forecast

--- weatherforecast/apps/weather/serializers.py
+++ weatherforecast/apps/weather/serializers.py
@@ -17,10 +17,10 @@ class WeatherQueryCreateSerializer(serializers.ModelSerializer):
     temperature = serializers.FloatField(required=True)
     condition = serializers.CharField(max_length=100, required=True)
     forecast_date = serializers.DateTimeField(required=True)
-    
+
     class Meta:
         model = WeatherQuery
         fields = [ 'location', 'forecast_date', 'temperature', 'condition' ]
     
     def validate_forecast_date(self, value):
-        return value.replace(minute = 0, second = 0, microsend = 0)
+        return value.replace(minute = 0, second = 0, microsecond = 0)
