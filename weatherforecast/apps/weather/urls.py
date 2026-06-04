f2b7711b4bc4f56a10420b04d5dce18775a670e1
guido <guido.brini99@gmail.com>
2026-06-04 17:42:55 +0200

    revisione urls

--- weatherforecast/apps/weather/urls.py
+++ weatherforecast/apps/weather/urls.py
@@ -4,7 +4,7 @@ from .views import WeatherForecastView, WeatherForecastDetailView, WeatherForeca
 urlpatterns = [
     path('forecast/', WeatherForecastView.as_view(), name='weather_forecast'),
     path('forecast/<int:pk>', WeatherForecastDetailView.as_view(), name='weather_forecast_detail'),
-    path('forecast/history', WeatherForecastQueryHistoryView.as_view(), name='weather_forecast_history'),
-    path('forecast/history/<int:pk>', WeatherForecastQueryHistoryDetailView.as_view(), name='weather_forecast_history_detail'),
+    path('forecast/history', WeatherForecastQueryHistoryView.as_view(), name='weather_forecast_query_history'),
+    path('forecast/history/<int:pk>', WeatherForecastQueryHistoryDetailView.as_view(), name='weather_forecast_query_history_detail'),
     path('forecast/tracking', WeatherForecastRequestTrackingView.as_view(), name='weather_forecast_tracking')
 ]
