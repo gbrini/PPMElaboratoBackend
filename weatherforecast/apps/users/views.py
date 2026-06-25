bcc563c08b7bc6a51b78fe6cd16c6ffa37f49372
guido <guido.brini99@gmail.com>
2026-06-25 19:55:32 +0200

    register

--- weatherforecast/apps/users/views.py
+++ weatherforecast/apps/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+from .serializer import RegisterSerializer
 
-# Create your views here.
+class RegisterView(APIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
