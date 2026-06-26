3993f31420a92ff831399def8cae95e23684eeff
guido <guido.brini99@gmail.com>
2026-06-26 20:01:31 +0200

    rev

--- weatherforecast/apps/users/serializers.py
+++ weatherforecast/apps/users/serializers.py
@@ -1,18 +1,19 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
+from .models import CustomUser
 
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
         required=True, 
-        validators=[UniqueValidator(queryset=User.objects.all())]
+        validators=[UniqueValidator(queryset=CustomUser.objects.all())]
     )
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
-        model = User
+        model = CustomUser
         fields = ( 'username', 'password', 'password2', 'email', 'first_name', 'last_name' )
 
     def valiate(self, attrs):
--- weatherforecast/apps/users/views.py
+++ weatherforecast/apps/users/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
+from .models import CustomUser
 from rest_framework.permissions import AllowAny
-from .serializer import RegisterSerializer
+from .serializers import RegisterSerializer
 
 class RegisterView(APIView):
-    queryset = User.objects.all()
+    queryset = CustomUser.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
