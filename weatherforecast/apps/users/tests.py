11ea50181be8b202de7fc2db6d495e09eddab614
guido <guido.brini99@gmail.com>
2026-06-30 21:23:30 +0200

    Inizio test

--- weatherforecast/apps/users/tests.py
+++ weatherforecast/apps/users/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class UserAccountTest(APITestCase):
+    def test_user_registration(self):
+        url = reverse('auth_register')
+        data = {
+            "username": "testuser",
+            "password": "PasswordTest123!",
+            "password2": "PasswordTest123!"
+        }
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(get_user_model().objects.count(), 1)
+        self.assertEqual(get_user_model().objects.get().username, "testuser")
