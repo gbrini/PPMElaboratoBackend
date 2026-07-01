25ac3b7d9982167cc938cb5904a6578614d8ed54
guido <guido.brini99@gmail.com>
2026-07-01 19:56:31 +0200

    revisione test user

--- weatherforecast/apps/users/tests.py
+++ weatherforecast/apps/users/tests.py
@@ -4,17 +4,37 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
 
+User = get_user_model()
+PASSWORD = "Password123!"
+
 class UserAccountTest(APITestCase):
+    def setUp(self):
+        self.register_url = reverse('auth_register')
+        self.login_url = reverse('token_obtain_pair')
+        self.refresh_url = reverse('token_refresh')
+
+        self.super_user = User.objects.create_superuser(username="superuser", password=PASSWORD)
+        self.user = User.objects.create_user(username="maintestuser", password=PASSWORD)
+
     def test_user_registration(self):
-        url = reverse('auth_register')
         data = {
             "username": "testuser",
-            "password": "PasswordTest123!",
-            "password2": "PasswordTest123!"
+            "password": PASSWORD,
+            "password2": PASSWORD
         }
 
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(self.register_url, data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(get_user_model().objects.count(), 1)
-        self.assertEqual(get_user_model().objects.get().username, "testuser")
+
+    def test_login_and_refresh(self):
+        login_data = { 'username': self.user.username, 'password': PASSWORD }
+        
+        login_response = self.client.post(self.login_url, login_data, format='json')
+        self.assertEqual(login_response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', login_response.data)
+
+        refresh_token = login_response.data['refresh']
+        refresh_response = self.client.post(self.refresh_url, { 'refresh': refresh_token }, format='json')
+        self.assertEqual(refresh_response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', refresh_response.data)
